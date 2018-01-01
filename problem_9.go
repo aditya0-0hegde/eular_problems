fc87a43dff82283f0eada95ccd1fb907fe386177
aditya0-0hegde <aditya0.0hegde@gmail.com>
2018-01-01 16:41:05 +0530

    pythogorean triplet with sum 1000

---
+++ problem_9.go
@@ -0,0 +1,18 @@
+package main
+import(
+    "fmt"
+)
+func main() {
+    for a:=1;a<1000;a++{
+        for b:=a+1;b<1000;b++{
+            for c:=b+1;c<=1000;c++{
+                if (a*a)+(b*b) == (c*c){
+                    if a+b+c==1000{
+                        fmt.Println(a,b,c)
+                        fmt.Println(a*b*c)
+                    }
+                }
+            }
+        }
+    }
+}
